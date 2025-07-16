3339beefa624853e9974240cc0e14432714c7279
vigneshrao07 <162863603+vigneshrao07@users.noreply.github.com>
2025-07-16 22:17:01 +0100

    commit

--- src/Component/Profile.jsx
+++ src/Component/Profile.jsx
@@ -1,15 +1,21 @@
- 
+import { FaFacebookF, FaTwitter, FaYoutube, FaInstagram } from "react-icons/fa";
 
- import { FaFacebookF, FaTwitter, FaYoutube, FaInstagram } from 'react-icons/fa';
- 
-
-const Profile = ({ name, title, image, bio, followers, likes, photos, backgroundImage }) => {
+const Profile = ({
+  name,
+  title,
+  image,
+  bio,
+  followers,
+  likes,
+  photos,
+  backgroundImage,
+}) => {
   return (
     <div className="max-w-sm mx-auto bg-white rounded-2xl shadow-lg overflow-hidden">
       {/* Top Half with Background Image */}
       <div
         className="relative h-35 bg-cover bg-center"
-         style={{ backgroundImage: `url(${backgroundImage})` }}
+        style={{ backgroundImage: `url(${backgroundImage})` }}
       >
         <img
           className="absolute bottom-[-40px] left-1/2 transform -translate-x-1/2 w-20 h-20  rounded-full border-4 border-white"
@@ -31,23 +37,44 @@ const Profile = ({ name, title, image, bio, followers, likes, photos, background
           <button className="bg-gray-200 text-gray-800 px-4 py-2 rounded-full hover:bg-gray-400 transition">
             Message
           </button>
-           <button className="bg-green-500 text-gray-800 px-4 py-2 rounded-full hover:bg-red-400 transition">
+          <button className="bg-green-500 text-gray-800 px-4 py-2 rounded-full hover:bg-red-400 transition">
             call
           </button>
         </div>
       </div>
 
       <div className="mt-4 mb-4 flex justify-center gap-6 text-2xl">
-        <a href="https://facebook.com" target="_blank" rel="noreferrer" className="text-blue-600 hover:scale-110 transition">
+        <a
+          href="https://facebook.com"
+          target="_blank"
+          rel="noreferrer"
+          className="text-blue-600 hover:bg-pink-300  rounded-4xl    "
+          
+        >
           <FaFacebookF />
         </a>
-        <a href="https://twitter.com" target="_blank" rel="noreferrer" className="text-sky-400 hover:scale-110 transition">
+        <a
+          href="https://twitter.com"
+          target="_blank"
+          rel="noreferrer"
+          className="text-sky-400  hover:bg-pink-300 rounded-4xl    "
+        >
           <FaTwitter />
         </a>
-        <a href="https://youtube.com" target="_blank" rel="noreferrer" className="text-red-600 hover:scale-110 transition">
+        <a
+          href="https://youtube.com"
+          target="_blank"
+          rel="noreferrer"
+          className="text-red-600   hover:bg-pink-300 rounded-4xl "
+        >
           <FaYoutube />
         </a>
-        <a href="https://instagram.com" target="_blank" rel="noreferrer" className="text-pink-500 hover:scale-110 transition">
+        <a
+          href="https://instagram.com"
+          target="_blank"
+          rel="noreferrer"
+          className="text-pink-500  hover:bg-pink-300 rounded-4xl  "
+        >
           <FaInstagram />
         </a>
       </div>
@@ -66,14 +93,8 @@ const Profile = ({ name, title, image, bio, followers, likes, photos, background
           <p>Photos</p>
         </div>
       </div>
-       
-
     </div>
   );
 };
 
 export default Profile;
-
-
-
- 
